f23e207ac7f68bd1d0fdc124aa655b7f92f3a619
floeisi17 <floeisi17@gmail,com>
2018-01-24 14:11:40 +0100

    flos zeug

--- src/app/ride-detail/ride-detail.component.ts
+++ src/app/ride-detail/ride-detail.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Person, Fahrt, Auto } from '../classes';
+import { DatePipe } from '@angular/common';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-ride-detail',
   templateUrl: './ride-detail.component.html',
   styleUrls: ['./ride-detail.component.css']
 })
+
 export class RideDetailComponent implements OnInit {
 
-  constructor() { }
+  private fahrten: Array<Fahrt>;
+  private id: number;
+  private fahrt: Fahrt;
+
+  constructor(private router: Router, private datePipe: DatePipe, private dataService: DataService) { 
+    this.fahrten = dataService.angeboteneFahrten;
+    //need id via navParams
+    this.id = 0;
+
+    this.fahrtAnzeigen();
+  }
 
   ngOnInit() {
   }
 
+  private fahrtAnzeigen(){
+    this.fahrt = this.fahrten[this.id];
+  }
+
+  anfrageSenden(){
+    
+  }
 }
