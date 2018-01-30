4b85c91927b66e2759b94ff7a7d41bc970793dbf
T0BiD <opayq-tobi-githuub@criztovyl.space>
2018-01-30 11:55:01 +0100

    check anmeldung

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { DataService } from '../data.service';
+import { LoginComponent } from '../login/login.component';
+import { MatDialog } from '@angular/material';
 
 @Component({
   selector: 'app-landing-page',
@@ -8,18 +11,35 @@ import { Router } from '@angular/router';
 })
 export class LandingPageComponent implements OnInit {
 
-  constructor(private router:Router) { }
+  constructor(private router: Router, private dataService: DataService, private dialog : MatDialog) { }
 
   ngOnInit() {
   }
 
-  suchen(){
+  suchen() {
     console.log("suche");
     this.router.navigate(['/suche']);
   }
 
-  anbieten(){
-    console.log("anbieten"); //link zur anbietenseite muss noch hinzugefügt werden
-    this.router.navigate(['/createride']);
+  anbieten() {
+    if (this.dataService.angemeldeterUser) {
+
+
+      console.log("anbieten"); //link zur anbietenseite muss noch hinzugefügt werden
+      this.router.navigate(['/createride']);
+    } else {
+      this.login();
+    }
+  }
+
+  login(): void {
+
+    let dialogRef = this.dialog.open(LoginComponent, {
+      data: {}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
   }
 }
